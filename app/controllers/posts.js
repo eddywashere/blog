0643fd57480d250324c85482d90881abb0ad0c5c
Eddy Hernandez <eddy.hernandez@rackspace.com>
2013-09-06 16:42:14 -0500

    adding update method to posts controller

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -54,6 +54,29 @@ exports.create = function(req, res){
   });
 };
 
+exports.update = function(req, res){
+  Post.findOne({ _id : req.params.id }, function(err, post){
+    if (err) {
+      return res.json(500, {error: err.stack});
+    } else if (!post){
+      return res.json(404, {message: 'Post not found'});
+    }
+
+    post.title = req.body.title;
+    post.body = req.body.body;
+    post.updated_at = Date.now();
+
+    post.save(function(err){
+      res.status(201);
+      res.format({
+        json: function(){
+          res.send({message: "Post successfully updated"});
+        }
+      });
+    });
+  });
+};
+
 exports.destroy = function(req, res){
   Post.findOne({ _id : req.params.id }, function(err, post){
     if (err) {
