d90943f521311558ca101489db138050922a57ad
Eddy Hernandez <eddy.hernandez@rackspace.com>
2013-09-05 23:18:48 -0500

    adding destroy method to posts controller

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -53,3 +53,21 @@ exports.create = function(req, res){
     }
   });
 };
+
+exports.destroy = function(req, res){
+  Post.findOne({ _id : req.params.id }, function(err, post){
+    if (err) {
+      return res.json(500, {error: err.stack});
+    } else if (!post){
+      return res.json(404, {message: 'Post not found'});
+    }
+    post.remove(function(err){
+      res.status(200);
+      res.format({
+        json: function(){
+          res.send({message: "Post successfully deleted"});
+        }
+      });
+    });
+  });
+};
